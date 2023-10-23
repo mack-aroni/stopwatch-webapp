fba18b10b192fc293f4dc55798a14ed33772f230
mack-aroni <etanm2013@gmail.com>
2023-10-23 19:33:21 -0400

    added button "pressed" logic

--- static/stopwatch.js
+++ static/stopwatch.js
@@ -4,49 +4,68 @@ const startBtn = document.querySelector("#startBtn");
 const pauseBtn = document.querySelector("#pauseBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
-let countdownTime = 3600; // var
-let currentTime = countdownTime; // initially displayed time
+let countdownTime = 3600; // Initial countdown time (in seconds)
+let currentTime = countdownTime; // Initially displayed time
 let paused = true;
 let intervalId;
 
-// on start button click execute updateTime every second
+// Function to set the active state for buttons
+function setActiveButton(activeButton) {
+  startBtn.classList.remove("active");
+  pauseBtn.classList.remove("active");
+  resetBtn.classList.remove("active");
+  if (activeButton != resetBtn) {
+    activeButton.classList.add("active");
+  }
+  else {
+    activeButton.classList.add("active");
+    activeButton.classList.remove("active");
+  }
+}
+
+// On start button click, execute updateTime every second
 startBtn.addEventListener("click", () => {
-    if (paused) {
-        paused = false;
-        intervalId = setInterval(updateTime, 1000);
-    }
+  if (paused) {
+    setActiveButton(startBtn);
+    paused = false;
+    intervalId = setInterval(updateTime, 1000);
+  }
 });
 
-// pauses timer on click at intervalId
+// On pause button click, pause the timer and set the active state
 pauseBtn.addEventListener("click", () => {
-    if (!paused) {
-        paused = true;
-        clearInterval(intervalId);
-    }
-});
-
-// resets timer
-resetBtn.addEventListener("click", () => {
+  setActiveButton(pauseBtn);
+  if (!paused) {
     paused = true;
     clearInterval(intervalId);
-    currentTime = countdownTime;
-    updateTime();
+  }
+});
+
+// On reset button click, reset the timer and set the active state
+resetBtn.addEventListener("click", () => {
+  setActiveButton(resetBtn);
+  paused = true;
+  clearInterval(intervalId);
+  currentTime = countdownTime;
+  updateTime();
+  // clears the document title
+  document.title = "Timer";
 });
 
 function updateTime() {
   if (!paused && currentTime > 0) {
-      currentTime--;
-      const hrs = pad(Math.floor(currentTime / 3600));
-      const mins = pad(Math.floor((currentTime % 3600) / 60));
-      const secs = pad(currentTime % 60);
-      timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
-      
-      // update the document title with the remaining time
-      document.title = `${hrs}:${mins}:${secs}`;
+    currentTime--;
+    const hrs = pad(Math.floor(currentTime / 3600));
+    const mins = pad(Math.floor((currentTime % 3600) / 60));
+    const secs = pad(currentTime % 60);
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+    
+    // Update the document title with the remaining time
+    document.title = `${hrs}:${mins}:${secs}`;
   }
 }
 
-
+// helper function
 function pad(unit) {
-    return unit < 10 ? `0${unit}` : unit;
+  return unit < 10 ? `0${unit}` : unit;
 }
